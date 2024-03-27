465c634023c97a4d09150e8acafe79762caff71c
Song Juhyun <juju6985@outlook.kr>
2024-03-27 16:00:19 +0900

    test open source model

--- test.py
+++ test.py
@@ -1,20 +1,31 @@
 ## Build a sample vectorDB
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Load blog post
-loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
+
+# Load data
+loader = TextLoader("./state_of_the_union.txt")
 data = loader.load()
 
 # Split
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-splits = text_splitter.split_documents(data)
+texts = text_splitter.split_documents(data)
 
-# VectorDB
-embedding = OpenAIEmbeddings()
-vectordb = Chroma.from_documents(documents=splits, embedding=embedding)
+embedding =  HuggingFaceEmbeddings()
+retriever = FAISS.from_documents(texts, embedding).as_retriever()
+
+docs = retriever.get_relevant_documents(
+    "What did the president say about Ketanji Brown Jackson"
+)
+pretty_print_docs(docs)
 
 
 
